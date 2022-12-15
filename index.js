d7b240bb11cce9d7036926d3d28426954d34688b
tony555t <tonytullah764@gmail.com>
2022-12-15 10:49:12 +0300

    js event lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+const dodgerWidth = parseInt(getComputedStyle(dodger).width.replace("px", ""));
+const game = document.getElementById("game");
+const gameWidth = parseInt(getComputedStyle(game).width.replace("px", ""));
+
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+//   if (left < gameWidth - dodgerWidth) {
+//     dodger.style.left = `${left + 1}px`;
+//   }
+    dodger.style.left = `${left + 1}px`;
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+
+  if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
